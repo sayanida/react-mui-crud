a5d36e21480aaeb90ecf6a43010aad3ae53611c0
sayanida <38n.01.aus@gmail.com>
2025-11-23 12:35:06 +1030

    Add grid table using DataGrid component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { DataGrid } from '@mui/x-data-grid';
+import { Paper } from '@mui/material';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+];
+
+const rows = [
+ { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper sx={{ height: 400, width: '100%' }}>
+        <DataGrid rows={rows}
+        columns={columns}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{border:0}}
+        />
+      </Paper>
     </div>
   );
 }
